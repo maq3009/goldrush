977628ad0d544fa31b63d829ea371917886cd30b
maq3009 <maq3009@hotmail.com>
2024-10-13 16:32:14 -0400

    7th commit

---
+++ lib/components/hud/score_text.dart
@@ -0,0 +1,6 @@
+import 'package:flame/components.dart';
+import 'package:flame/input.dart';
+import 'package:flame/palette.dart';
+import 'package:flutter/cupertino.dart';
+
+class ScoreText extends HudMarginComponent {}
